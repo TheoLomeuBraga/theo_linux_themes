1574c94c44708d628898fd93ec7a582b4fe15cc2
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-22 13:30:52 -0300

    a

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,12 +3,44 @@
 theme=${1}
 user=${2}
 
+install_dependencies(){
+    sudo apt -y install software-properties-common
+
+    dependencies="configs/${theme}/dependencies.txt"
+    n = 1
+    while read line; do
+        sudo add-apt-repository ${line}
+        n =$ ( ( n+ 1 ) )
+    done < $dependencies
+
+    sudo apt update
+
+    repositorys="configs/${theme}/repositorys.txt"
+    n = 1
+    while read line; do
+        sudo apt -y install ${line}
+        n =$ ( ( n+ 1 ) )
+    done < $repositorys
+}
+
 install_configs(){
     echo instaling theme: ${theme}
     echo to user: ${user}
+    config_folders="configs/${theme}/config_folders.txt"
+    n = 1
+    while read line; do
+        sudo cp -r /configs/${theme}/${line} /home/${user}/.config
+        n =$ ( ( n+ 1 ) )
+    done < $config_folders
+    install_dependencies
 }
 
-sudo cp -r /theo_themes /usr/share
+install(){
+    sudo cp -r /theo_themes /usr/share
+    install_configs
+}
 
-install_configs
+
+install
+
 
